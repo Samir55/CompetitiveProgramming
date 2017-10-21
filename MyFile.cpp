9e243abab50a1b12146287760b34137a67a6a82b
Samir55 <discere044@icloud.com>
2017-10-21 19:05:52 +0200

    Add Dijkstra to MyFile.cpp's collection.

--- MyFile.cpp
+++ MyFile.cpp
@@ -10,8 +10,9 @@
 using namespace std;
 #define lp(i, n)        for(int i=0;i<(int)(n);++i)
 
-bool vis[10];
+int vis[10];
 vector< vector<int> > g;
+vector< vector<pair<int, int>>> gcost;
 
 void bfs(int src) {
 	vis[src] = true;
@@ -35,6 +36,34 @@ void dfs(int i, int n) {
 	lp(j, g[i].size()) {
 		if (!vis[g[i][j]]) {
 			dfs(j, n);
+		}
+	}
+}
+
+struct node {
+	int idx;
+	int cost;
+	node(int i, int c) : idx(i), cost(c) {}
+	bool operator < (const node& x) const { // "<" maxheap;
+		return cost > x.cost;
+	}
+};
+
+void Dijkstra(int src, int dest = -1000) {
+	priority_queue<node> q;
+	q.push(node(src, 0));
+	while (!q.empty()) {
+		node x = q.top(); 
+		q.pop();
+		if (vis[x.idx] != -1) continue; // No better result will be found afterwards.
+		vis[x.idx] = x.cost;
+		//if(dest != -100 && x.idx == dest) return x.cost;
+		// Push children.
+		lp(i, gcost[x.idx].size()) {
+			int to = gcost[x.idx][i].first;
+			int cost = gcost[x.idx][i].second;
+			if (vis[to] == -1) 
+				q.push(node(to, x.cost + cost));
 		}
 	}
 }
