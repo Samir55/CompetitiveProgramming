22cbea2e490bfb71eae9a368220854f3391454e0
Samir55 <discere044@icloud.com>
2017-11-03 22:25:35 +0200

    Add GCD the largest problem.

---
+++ UVA/GCD The Largest.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <map>
+#include <cmath>
+#include <cstring>
+using namespace std;
+#define lp(i, n)        for(int i=0;i<(int)(n);++i)
+
+long long int x;
+int res = 0;
+
+int main() {
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    int t;
+    cin >> t;
+    int k = 1;
+    while (t--) {
+        cin >> x;
+        long long a, b;
+        a = x/2; //(x-2)/2 +1 = x/2 - 1+ 1 = x/2;
+        cout << a << endl;
+    }
+    return 0;
+}
